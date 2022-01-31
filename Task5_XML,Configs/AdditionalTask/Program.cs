8a152cfb0e2e3fcb6f34b6bbe7501f5d1015118a
Bohdan Bondar <bv.bondar777@gmail.com>
2022-01-31 22:57:45 +0200

    lesson 5 done, except task 4

---
+++ Task5_XML,Configs/AdditionalTask/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Xml;
+
+namespace AdditionalTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var document = new XmlTextWriter("TelephoneBook.xml", null);
+
+            document.Formatting = Formatting.Indented;
+            document.IndentChar = '\t';
+            document.Indentation = 1;
+
+            document.WriteStartDocument();
+            document.WriteStartElement("MyContacts");
+            document.WriteStartElement("Contact");
+            document.WriteStartAttribute("TelephoneNumber");
+            document.WriteString("0441234567");
+            document.WriteEndAttribute();
+            document.WriteString("Name Example");
+            document.WriteEndElement();
+            document.WriteEndElement();
+
+            document.Close();
+
+        }
+    }
+}
---
+++ Task5_XML,Configs/Task2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Xml;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlTextReader reader = new XmlTextReader("bench.xml");
+
+            while (reader.Read())
+            {
+                Console.WriteLine($"NodeType: {reader.NodeType.ToString()}");
+                Console.WriteLine($"Name: {reader.Name}");
+                Console.WriteLine($"Value: {reader.Value}");
+            }
+
+            reader.Close();
+            Console.ReadKey();
+        }
+    }
+}
